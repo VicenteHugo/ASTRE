05ffd86b44cdc75f0fd6e3c081627749d2d7da46
Ottirate <sarah.hautot76@gmail.com>
2023-12-11 15:54:35 +0100

    Courage Oscar

---
+++ src/model/modules/Ressource.java
@@ -0,0 +1,58 @@
+package model.modules;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import model.Semestres;
+
+public class Ressource extends Module {
+
+	private String type;
+
+	private int nbHeureTotal;
+	private List<Integer> listeNombreHeurePn;
+	private HashMap<Integer, Integer> listeNombreHeure;
+
+	private int heurePonctuel;
+
+	private int nombreHeurePnTotal;
+	private int nombreHeureAffecte;
+
+	public Ressource(String libelleLongModule, String libelleCourtModule, Semestres semestres, int nbEtudiant,
+			boolean valide,
+			int nbGroupeTD, int nbGroupeTP, int nbHeureCMParSemaine, int nbHeureTDParSemaine,
+			int nbHeureTPParSemaine, int nbHeureTutParSemaine, int nbSemaineCM, int nbSemaineTD, int nbSemaineTP,
+			int nbSemaineTut, int nbHeureCM_PN, int nbHeureTD_PN, int nbHeureTut_PN, int nbHeureTP_PN,
+			int nbHeurePonctuel) {
+
+		super(libelleLongModule, libelleCourtModule, semestres, nbEtudiant, nbGroupeTD, nbGroupeTP, valide);
+		this.listeNombreHeurePn = new ArrayList<Integer>();
+		this.listeNombreHeure = new HashMap<Integer, Integer>();
+
+		this.initListHeurePn(nbHeureCM_PN, nbHeureTD_PN, nbHeureTP_PN, nbHeureTut_PN);
+		this.initNombreHeureSemaine(nbHeureCMParSemaine, nbHeureTDParSemaine, nbHeureTPParSemaine, nbHeureTutParSemaine,
+				nbSemaineCM, nbSemaineTD, nbSemaineTP, nbSemaineTut);
+
+		this.heurePonctuel = heurePonctuel;
+
+	}
+
+	public void initListHeurePn(int nbHeureCM_PN, int nbHeureTD_PN, int nbHeureTP_PN, int nbHeureTut_PN) {
+		this.listeNombreHeurePn.add(nbHeureCM_PN);
+		this.listeNombreHeurePn.add(nbHeureTD_PN);
+		this.listeNombreHeurePn.add(nbHeureTP_PN);
+		this.listeNombreHeurePn.add(nbHeureTut_PN);
+	}
+
+	public void initNombreHeureSemaine(int nbHeureCMParSemaine, int nbHeureTDParSemaine,
+			int nbHeureTPParSemaine, int nbHeureTutParSemaine, int nbSemaineCM, int nbSemaineTD, int nbSemaineTP,
+			int nbSemaineTut) {
+		this.listeNombreHeure.put(nbSemaineCM, nbHeureCMParSemaine);
+		this.listeNombreHeure.put(nbSemaineTD, nbHeureTDParSemaine);
+		this.listeNombreHeure.put(nbSemaineTP, nbHeureTPParSemaine);
+		this.listeNombreHeure.put(nbSemaineTut, nbHeureTutParSemaine);
+	}
+
+}
