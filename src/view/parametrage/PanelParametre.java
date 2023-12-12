1855ece3e69806033ca101ee91d69c76e754ada8
FailureUsername <adrianchalansonnet@gmail.com>
2023-12-12 14:28:50 +0100

    MAJ Bouton Ihm

--- src/view/parametrage/PanelParametre.java
+++ src/view/parametrage/PanelParametre.java
@@ -1,69 +1,76 @@
 package view.parametrage;
 
 import javax.swing.*;
-
-import view.accueil.FrameAccueil;
-import view.accueil.PanelAccueil;
-
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class PanelParametre extends JPanel implements ActionListener{
+import view.accueil.FrameAccueil;
+import view.accueil.PanelAccueil;
 
-	private JButton btnIntervenants;
-	private JButton btnHeures;
-	private JButton btnAcceuil;
+public class PanelParametre extends JPanel implements ActionListener {
 
-	private FrameAccueil frame;
-	
-	public PanelParametre(FrameAccueil frame) {
+    private JButton btnIntervenants;
+    private JButton btnHeures;
+    private JButton btnAccueil;
 
-		//Frame
-		frame.setTitle("Astre - Paramètre (Accueil)");
-		frame.setMinimumSize(new Dimension(400, 400));
-		this.frame = frame;
+    private FrameAccueil frame;
 
+    public PanelParametre(FrameAccueil frame) {
 
-		//Création 
-		this.btnIntervenants = new JButton("Catégorie intervenants");
-		this.btnHeures       = new JButton("Catégorie heures");
-		this.btnAcceuil      = new JButton("Accueil");
-		
-		//Layout
-		this.setLayout(new GridBagLayout());
-		GridBagConstraints gbc = new GridBagConstraints();
+        // Frame
+        frame.setTitle("Astre - Paramètres (Accueil)");
+        frame.setMinimumSize(new Dimension(400, 400));
+        this.frame = frame;
 
-		//Positionnement
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(10, 10, 10, 10);
+        // Création des composants
+        this.btnIntervenants = new JButton("Catégorie Intervenants");
+        this.btnHeures = new JButton("Catégorie Heures");
+        this.btnAccueil = new JButton("Accueil");
 
-		this.add(this.btnIntervenants, gbc);
+        // Ajustement de la taille des boutons
+        Dimension buttonSize = new Dimension(200, 50);
+        this.btnIntervenants.setPreferredSize(buttonSize);
+        this.btnHeures.setPreferredSize(buttonSize);
+        this.btnAccueil.setPreferredSize(buttonSize);
 
-		gbc.gridy++;
-		this.add(this.btnHeures, gbc);
+        // Ajustement de la police des boutons
+        Font buttonFont = new Font("Arial", Font.PLAIN, 16);
+        this.btnIntervenants.setFont(buttonFont);
+        this.btnHeures.setFont(buttonFont);
+        this.btnAccueil.setFont(buttonFont);
 
-		gbc.gridy++;
-		this.add(this.btnAcceuil, gbc);
+        // Layout
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
 
+        // Positionnement
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(20, 20, 20, 20);
 
-		//Action
-		this.btnIntervenants.addActionListener(this);
-		this.btnHeures      .addActionListener(this);
-		this.btnAcceuil     .addActionListener(this);
-	}
+        this.add(this.btnIntervenants, gbc);
 
-	public void actionPerformed(ActionEvent e) 
-	{
-		if (e.getSource() == this.btnIntervenants)
-			this.frame.changePanel(new PanelIntPara(this.frame));
+        gbc.gridy++;
+        this.add(this.btnHeures, gbc);
 
-		if (e.getSource() == this.btnHeures)
-			this.frame.changePanel(new PanelHeurePara(this.frame));
+        gbc.gridy++;
+        this.add(this.btnAccueil, gbc);
 
-		if (e.getSource() == this.btnAcceuil)
-			this.frame.changePanel(new PanelAccueil(this.frame));
-	}
-	
+        // Action
+        this.btnIntervenants.addActionListener(this);
+        this.btnHeures.addActionListener(this);
+        this.btnAccueil.addActionListener(this);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == this.btnIntervenants)
+            this.frame.changePanel(new PanelIntPara(this.frame));
+
+        if (e.getSource() == this.btnHeures)
+            this.frame.changePanel(new PanelHeurePara(this.frame));
+
+        if (e.getSource() == this.btnAccueil)
+            this.frame.changePanel(new PanelAccueil(this.frame));
+    }
 }
