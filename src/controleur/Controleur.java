b20cbb60eede38354466395308ddfd58d8a9e81b
OcsCgn <Oscar.cocagne@free.fr>
2023-12-13 14:25:18 +0100

    modifications controleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -12,22 +12,23 @@ import model.Semestres;
 import model.modules.Module;
 
 public class Controleur {
-    private static Controleur controleur;
-    private FrameAccueil frameAccueil;
+	private static Controleur controleur;
+	private FrameAccueil frameAccueil;
 
-    public static Controleur creerControleur() {
-        if(controleur == null)
-            controleur = new Controleur();
-        
-        return controleur;
-    }  
+	public static Controleur creerControleur() {
+		if (controleur == null)
+			controleur = new Controleur();
 
-    public static Controleur getControleur() {return Controleur.controleur;}
-
-    private Controleur() {
-		this.frameAccueil = new FrameAccueil();
+		return controleur;
 	}
 
+	public static Controleur getControleur() {
+		return Controleur.controleur;
+	}
+
+	private Controleur() {
+		this.frameAccueil = new FrameAccueil();
+	}
 
 	public ArrayList<CategorieIntervenant> getCategorieIntervenants() {
 		return Etat.getCategoriesIntervenants();
@@ -45,11 +46,26 @@ public class Controleur {
 		return Etat.getModules();
 	}
 
+	public ArrayList<Module> getModules(Semestres semestres) {
+		ArrayList<Module> retour = new ArrayList<>();
+		for (Module m : Etat.getModules()) {
+			if (m.getSemestres().equals(semestres)) {
+				retour.add(m);
+			}
+		}
+		return retour;
+	}
+
+	public int getNbModule() {
+		return Etat.getModules().size();
+	}
+
 	public ArrayList<Semestres> getSemestres() {
 		return Etat.getSemestres();
 	}
 
-    public static void main(String[] args) {
-        Controleur.creerControleur();
-    }
+	public static void main(String[] args) {
+		Controleur.creerControleur();
+	}
+
 }
