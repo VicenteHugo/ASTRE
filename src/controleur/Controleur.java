f0902dadc8bb8c9e52c7f03bac5e6ea4a2595ba3
OcsCgn <Oscar.cocagne@free.fr>
2023-12-13 14:13:00 +0100

    controleur ajout get

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,5 +1,42 @@
 package controleur;
 
+import view.accueil.FrameAccueil;
+
+import java.util.ArrayList;
+
+import model.CategorieHeures;
+import model.CategorieIntervenant;
+import model.Etat;
+import model.Intervenants;
+import model.Semestres;
+import model.modules.Module;
+
 public class Controleur {
-    
+
+	private FrameAccueil frameAccueil;
+
+	public Controleur() {
+		this.frameAccueil = new FrameAccueil();
+
+	}
+
+	public ArrayList<CategorieIntervenant> getCategorieIntervenants() {
+		return Etat.getCategoriesIntervenants();
+	}
+
+	public ArrayList<CategorieHeures> getCategorieHeures() {
+		return Etat.getCategoriesHeures();
+	}
+
+	public ArrayList<Intervenants> getIntervenants() {
+		return Etat.getIntervenants();
+	}
+
+	public ArrayList<Module> getModules() {
+		return Etat.getModules();
+	}
+
+	public ArrayList<Semestres> getSemestres() {
+		return Etat.getSemestres();
+	}
 }
--- src/view/parametrage/GrilleCatInt.java
+++ src/view/parametrage/GrilleCatInt.java
@@ -5,50 +5,66 @@ import java.util.List;
 import javax.swing.table.AbstractTableModel;
 
 import controleur.Controleur;
-import model.CategorieIntervenants;
+import model.CategorieIntervenant;
 
 public class GrilleCatInt extends AbstractTableModel {
 
-	private String[]   tabEntetes;
+	private String[] tabEntetes;
 	private Object[][] tabDonnees;
 
 	public GrilleCatInt() {
-		
-		CategorieIntervenants cat;
-		this.tabDonnees = new Object[][] {{ "info_ec" ,"lib", 1.0 , 200, 250},
-		                                  { "vaca_pro","lib", 1.0 , 200, 250},
-		                                  { "vac_sd"  ,"lib", 1.0 , 200, 250},
-		                                  { "vaca_ret","lib", 1.0 , 200, 250},
-		                                  { "info_sd" ,"lib", 1.0 , 200, 250} };
+
+		CategorieIntervenant cat;
+		this.tabDonnees = new Object[][] { { "info_ec", "lib", 1.0, 200, 250 },
+				{ "vaca_pro", "lib", 1.0, 200, 250 },
+				{ "vac_sd", "lib", 1.0, 200, 250 },
+				{ "vaca_ret", "lib", 1.0, 200, 250 },
+				{ "info_sd", "lib", 1.0, 200, 250 } };
 
 		/*
-		List<CategorieIntervenants> lstClients = ctrl.getCatInt();
-		
-		tabDonnees = new Object[lstClients.size()][4];
+		 * List<CategorieIntervenants> lstClients = ctrl.getCatInt();
+		 * 
+		 * tabDonnees = new Object[lstClients.size()][4];
+		 * 
+		 * for ( int lig=0; lig<lstClients.size(); lig++)
+		 * {
+		 * cat = lstClients.get(lig);
+		 * 
+		 * tabDonnees[lig][0] = cat.getlibCatInt ();
+		 * tabDonnees[lig][1] = cat.getCoefCatInt ();
+		 * tabDonnees[lig][2] = cat.getHeureMinCatInt();
+		 * tabDonnees[lig][3] = cat.getHeureMaxCatInt();
+		 * }
+		 */
 
-		for ( int lig=0; lig<lstClients.size(); lig++)
-		{
-			cat = lstClients.get(lig);
-
-			tabDonnees[lig][0] = cat.getlibCatInt     ();
-			tabDonnees[lig][1] = cat.getCoefCatInt    ();
-			tabDonnees[lig][2] = cat.getHeureMinCatInt();
-			tabDonnees[lig][3] = cat.getHeureMaxCatInt();
-		}*/
-
-		this.tabEntetes = new String[]   {"Code","Libellé", "Coef", "Heures minimum", "Heures maximum"};
+		this.tabEntetes = new String[] { "Code", "Libellé", "Coef", "Heures minimum", "Heures maximum" };
 
 	}
 
-	public int    getRowCount   () {return this.tabDonnees.length;}
-	public int    getColumnCount() {return this.tabEntetes.length;}
-	
-	public String getColumnName (int col)          { return this.tabEntetes[col];          }
-	public Object getValueAt    (int row, int col) { return this.tabDonnees[row][col];     }
-	public Class  getColumnClass(int c)            {return getValueAt(0, c).getClass();}
-	
-	public boolean isCellEditable(int row, int col) { return true;}
-	
+	public int getRowCount() {
+		return this.tabDonnees.length;
+	}
+
+	public int getColumnCount() {
+		return this.tabEntetes.length;
+	}
+
+	public String getColumnName(int col) {
+		return this.tabEntetes[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		return this.tabDonnees[row][col];
+	}
+
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	public boolean isCellEditable(int row, int col) {
+		return true;
+	}
+
 	public void setValueAt(Object value, int row, int col) {
 
 		this.tabDonnees[row][col] = value;
