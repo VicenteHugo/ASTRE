172172a495043a60a1c45c38020bbd33bf1589b8
OcsCgn <Oscar.cocagne@free.fr>
2023-12-13 10:54:45 +0100

    finition module

--- src/model/modules/Ressource.java
+++ src/model/modules/Ressource.java
@@ -4,59 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.CategorieHeures;
-import model.Etat;
 import model.Semestres;
 
 public class Ressource extends Module {
 
-	private List<Integer> listElementCM;
-	private List<Integer> listElementTD;
-	private List<Integer> listElementTP;
-	private List<Integer> listElementTut;
-	private Etat etat;
-
 	public Ressource(Semestres semestres, String code, String libLong, String libCourt, int heurePonctuel) {
-
 		super(semestres, code, libLong, libCourt, heurePonctuel);
-
-		this.listElementCM = new ArrayList<Integer>();
-		this.listElementTD = new ArrayList<Integer>();
-		this.listElementTP = new ArrayList<Integer>();
-
 	}
 
 	@Override
 	public void initList(int heurePN, int nbSemaine, int heureSemaine, int heurePonctuel, CategorieHeures catH) {
-		List<Integer> list = null;
-		CategorieHeures cH = catH;
 
-		String lib = catH.getlibCatHeur();
-		
-
-
-		switch (lib) {
-			case "CM":
-				list = this.listElementCM;
-				break;
-
-			case "TD":
-				list = this.listElementTD;
-				break;
-
-			case "TP":
-				list = this.listElementTP;
-				break;
-
-			case "H tut":
-				list = this.listElementTut;
-				break;
-		}
+		List<Integer> list = new ArrayList<>();
 
 		list.add(heurePN);
 		list.add(nbSemaine);
 		list.add(heureSemaine);
 		list.add(heurePonctuel);
 
-		this.heures.put(cH, list);
+		this.listCategorieHeure.add(catH);
+		this.heures.put(catH, list);
 	}
 }
--- src/model/modules/Sae.java
+++ src/model/modules/Sae.java
@@ -4,49 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.CategorieHeures;
-import model.Etat;
 import model.Semestres;
 
 public class Sae extends Module {
 
-	private List<Integer> listElementSae;
-	private List<Integer> listElementTut;
-
 	public Sae(Semestres semestres, String code, String libLong, String libCourt, int heurePonctuel) {
 		super(semestres, code, libLong, libCourt, heurePonctuel);
-		this.listElementSae = new ArrayList<Integer>();
-		this.listElementTut = new ArrayList<Integer>();
 	}
 
 	public void initList(int heurePNCM, int nbSemaineCM, int heureSemaineCM, int heurePonctuel, CategorieHeures catH) {
 
-		List<Integer> list = null;
-		CategorieHeures cH = catH;
-
-		String lib = cH.getlibCatHeur();
-
-		switch (lib) {
-			case "H Saé":
-				list = this.listElementSae;
-				break;
-
-			case "H tut":
-				list = this.listElementTut;
-				break;
-		}
-
-		for (CategorieHeures catHeure : Etat.getCategoriesHeures()) {
-			if (lib.equals(catHeure.getlibCatHeur())) {
-				cH = catHeure;
-				break;
-			}
-		}
-
+		List<Integer> list = new ArrayList<>();
 		list.add(heurePNCM);
 		list.add(nbSemaineCM);
 		list.add(heureSemaineCM);
 		list.add(heurePonctuel);
-		this.heures.put(cH, list);
+		this.listCategorieHeure.add(catH);
+		this.heures.put(catH, list);
 	}
 
 }
