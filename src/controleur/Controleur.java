8ca19e16d51c5784c78435b5aa8081869c3902cd
VicenteHugo <hugovicente965@gmail.com>
2023-12-13 14:14:02 +0100

    factory Controleur

--- src/controleur/Controleur.java
+++ src/controleur/Controleur.java
@@ -1,5 +1,19 @@
 package controleur;
 
 public class Controleur {
-    
+    private static Controleur controleur;
+
+    public static Controleur creerControleur() {
+        if(controleur == null)
+            controleur = new Controleur();
+        
+        return controleur;
+    }  
+
+    public static Controleur getControleur() {return Controleur.controleur;}
+
+
+    public static void main(String[] args) {
+        Controleur.creerControleur();
+    }
 }
