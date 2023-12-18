40836454bc7be709ff405a4f86412ec1c6cccf99
OcsCgn <Oscar.cocagne@free.fr>
2023-12-18 14:40:59 +0100

    modification

--- src/view/module/PanelRessources.java
+++ src/view/module/PanelRessources.java
@@ -599,7 +599,7 @@ public class PanelRessources extends JPanel implements ActionListener{
 
 	public PanelRessources (FrameAccueil frame, Module m) {
 
-		this (frame, m.getSemestres(),m.getSemestres());
+		this (frame, m.getSemestres());
 		this.mod = m;
 
 		//txtCode
