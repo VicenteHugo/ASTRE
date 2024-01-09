2d5ea69af57ad9575975b9777c44b8d870bf1cf7
Ottirate <sarah.hautot76@gmail.com>
2024-01-09 09:08:46 +0100

    Old JTextFieldNumber

--- src/view/JTextFieldNumber.java
+++ src/view/JTextFieldNumber.java
@@ -6,51 +6,44 @@ import java.awt.event.KeyListener;
 import javax.swing.JTextField;
 
 public class JTextFieldNumber extends JTextField implements KeyListener {
-    private boolean isFloat = false;
+	private boolean isFloat = false;
 
-    public JTextFieldNumber() {
-        super();
-        this.addKeyListener(this);
-    }
+	public JTextFieldNumber() {
+		super();
+		this.addKeyListener(this);
+	}
 
-    public JTextFieldNumber(int ind) {
-        super(ind);
-        this.addKeyListener(this);
-    }
+	public JTextFieldNumber(int ind) {
+		super(ind);
+		this.addKeyListener(this);
+	}
 
-    public JTextFieldNumber(String message) {
-        super(message);
-        this.addKeyListener(this);
-    }
+	public JTextFieldNumber(String message) {
+		super(message);
+		this.addKeyListener(this);
+	}
 
-    public JTextFieldNumber(String message, int ind) {
-        super(message, ind);
-        this.addKeyListener(this);
-    }
+	public JTextFieldNumber(String message, int ind) {
+		super(message, ind);
+		this.addKeyListener(this);
+	}
 
-    public void setFloat(boolean isFloat) {
-        this.isFloat = isFloat;
-    }
+	public void setFloat(boolean isFloat) {
+		this.isFloat = isFloat;
+	}
 
-    @Override
-    public void keyTyped(KeyEvent e) {
-        boolean isPointOrComma = (e.getKeyChar() == '.' || e.getKeyChar() == ',');
-        boolean containsPointOrComma = (this.getText().contains(",") || this.getText().contains("."));
+	@Override
+	public void keyTyped(KeyEvent e) {
+		boolean isPoint = false;
+		if(this.isFloat) isPoint = (e.getKeyChar() == '.' || e.getKeyChar() == ',');
+			
+		if (!Character.isDigit(e.getKeyChar()) && !isPoint)
+			e.consume();
+	}
 
-        if (this.isFloat) {
-            if (isPointOrComma && containsPointOrComma) {
-                e.consume();
-            } else if (!Character.isDigit(e.getKeyChar()) && !isPointOrComma) {
-                e.consume();
-            }
-        } else {
-            if (isPointOrComma) {
-                e.consume();
-            }
-        }
-    }
+	public void keyPressed(KeyEvent e) {}
 
-    public void keyPressed(KeyEvent e) {}
+	public void keyReleased(KeyEvent e) {}
 
-    public void keyReleased(KeyEvent e) {}
 }
+
